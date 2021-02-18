a5f7fdd77b34896ac37527827dd9547cde3002ad
Jeferson Arango ÃLópez <jeferson.arango@ucaldas.edu.co>
2021-02-18 08:21:31 -0500

    Modificación de la función ping en el controlador correspondiente

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -46,7 +46,7 @@ export class PingController {
   ping(): object {
     // Reply with a greeting, the current time, the url, and request headers
     return {
-      greeting: 'Hello from LoopBack - Prog3-G02',
+      greeting: 'Hello from LoopBack - Prog3-G02 modified',
       date: new Date(),
       url: this.req.url,
       headers: Object.assign({}, this.req.headers),
